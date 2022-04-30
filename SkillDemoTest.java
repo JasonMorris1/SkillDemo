46b3ca750de03b2255610e3433cd818e6d6e09a7
JasonMorris1 <jasonbm123@gmail.com>
2022-04-30 14:49:13 -0700

    added test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -1,1 +1,18 @@
 
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SkillDemoTest{
+
+    @Test
+    public void twoSumTest()
+    {
+
+        int[] input = {2,7,11,15};
+        int target = 12;
+        int[] expected = {0, 1};
+        assertArrayEquals(expected, SkillDemo.twoSum(input, target));
+    
+    }
+
+}

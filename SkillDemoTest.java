7d18ef49d89aaabdad5fca9939937a9033b309d0
Jason B Morris <j7morris@ucsd.edu>
2022-04-30 14:55:38 -0700

    fixed test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -9,7 +9,7 @@ public class SkillDemoTest{
     {
 
         int[] input = {2,7,11,15};
-        int target = 12;
+        int target = 9;
         int[] expected = {0, 1};
         assertArrayEquals(expected, SkillDemo.twoSum(input, target));
     
